adac0c3ba66c9f6b9bc3dc15c0406c32b2145811
kalasjonte <81684659+kalasjonte@users.noreply.github.com>
2021-09-14 14:36:03 +0200

    bildspel

--- index.js
+++ index.js
@@ -9,21 +9,64 @@ $( document ).ready(function() {
     $('#bild4').attr('src', pics[3]) 
     }
     setBilder()
-    $('#test').on('click', bildspel)
+
+    $('#nextImage').on('click', bildspel)
     var index = 0;
     
     function bildspel(e){
+        console.log(index)
+        if (index > pics.length) {
+            index = 0;
+        }
         if( index == 0){
-        $('.bildspel').fadeOut(3000)
-        $('#bild1').attr('src', pics[index + 1])
-        $('#bild2').attr('src', pics[(index + 2)])
-        $('#bild3').attr('src', pics[index + 3])
-        $('#bild4').attr('src', pics[index]) 
+        $('.bildspel').fadeOut(3000 , function() {   
+        $('#bild1').attr('src', pics[3])
+        $('#bild2').attr('src', pics[0])
+        $('#bild3').attr('src', pics[1])
+        $('#bild4').attr('src', pics[2]) 
         $('.bildspel').fadeIn(3000)
-        }
+        })
         index++;
-        }
         
+        } 
+        else if ( index == 1){
+         
+            $('.bildspel').fadeOut(3000 , function() {
+            $('#bild1').attr('src', pics[2])
+            $('#bild2').attr('src', pics[3])
+            $('#bild3').attr('src', pics[0])
+            $('#bild4').attr('src', pics[1]) 
+            $('.bildspel').fadeIn(3000)
+            })
+            index++;
+            
+            }
+            else if ( index == 2){
+         
+                $('.bildspel').fadeOut(3000 , function() {
+                $('#bild1').attr('src', pics[1])
+                $('#bild2').attr('src', pics[2])
+                $('#bild3').attr('src', pics[3])
+                $('#bild4').attr('src', pics[0]) 
+                $('.bildspel').fadeIn(3000)
+                })
+                index++;
+                
+                }    
+            else if ( index == 3){
+                    $('.bildspel').fadeOut(3000 , function() {
+                    $('#bild1').attr('src', pics[0])
+                    $('#bild2').attr('src', pics[1])
+                    $('#bild3').attr('src', pics[2])
+                    $('#bild4').attr('src', pics[3]) 
+                    $('.bildspel').fadeIn(3000)
+                    })
+                    index= 0;
+                    
+                    }    
+    }
 
-    
+    setInterval(bildspel, 7000)
+
+        
 })
