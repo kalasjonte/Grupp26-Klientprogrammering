be5303219c35fbcea0205433a0f0ac48110d046c
kalasjonte <81684659+kalasjonte@users.noreply.github.com>
2021-09-23 12:01:12 +0200

    bildspel ändat till en bild

--- index.js
+++ index.js
@@ -9,34 +9,15 @@ $( document ).ready(function() {
     
     var pics = ["images/1.jpg", "images/2.webp", "images/3.jpg", "images/4.jpg"]
 
-    function setBilder() {
-    $('#bild1').attr('src', pics[0])
-    $('#bild2').attr('src', pics[1])
-    $('#bild3').attr('src', pics[2])
-    $('#bild4').attr('src', pics[3]) 
-    }
-    setBilder()
-
     $('#nextImage').on('click', start)
     var index = 0;
     
     function bildspel(e){
-        console.log(index)
-        let width = window.innerWidth;
-        if(width> 768) {
-            // var img1 = $("<img></img>");
-            // img1.attr('id', 'bild1')
-            // img1.addClass('bildspel')
-            // setBilder();
-            // $("#bilder").append(img1);
         if (index > pics.length) {
             index = 0;
         }
         if( index == 0){
         $('.bildspel').fadeOut(3000 , function() {   
-        $('#bild1').attr('src', pics[3])
-        $('#bild2').attr('src', pics[0])
-        $('#bild3').attr('src', pics[1])
         $('#bild4').attr('src', pics[2]) 
         $('.bildspel').fadeIn(3000)
         })
@@ -47,9 +28,6 @@ $( document ).ready(function() {
         else if ( index == 1){
          
             $('.bildspel').fadeOut(3000 , function() {
-            $('#bild1').attr('src', pics[2])
-            $('#bild2').attr('src', pics[3])
-            $('#bild3').attr('src', pics[0])
             $('#bild4').attr('src', pics[1]) 
             $('.bildspel').fadeIn(3000)
             })
@@ -59,9 +37,6 @@ $( document ).ready(function() {
             else if ( index == 2){
          
                 $('.bildspel').fadeOut(3000 , function() {
-                $('#bild1').attr('src', pics[1])
-                $('#bild2').attr('src', pics[2])
-                $('#bild3').attr('src', pics[3])
                 $('#bild4').attr('src', pics[0]) 
                 $('.bildspel').fadeIn(3000)
                 })
@@ -70,57 +45,13 @@ $( document ).ready(function() {
                 }    
             else if ( index == 3){
                     $('.bildspel').fadeOut(3000 , function() {
-                    $('#bild1').attr('src', pics[0])
-                    $('#bild2').attr('src', pics[1])
-                    $('#bild3').attr('src', pics[2])
                     $('#bild4').attr('src', pics[3]) 
                     $('.bildspel').fadeIn(3000)
                     })
                     index= 0;
                     
                     }    
-                }
-        else if (width < 768) {
-            console.log('else if')
-            if (index > pics.length) {
-                index = 0;
-            }
-            if( index == 0){
-            $('#bild4').fadeOut(3000 , function() {   
-            $('#bild4').attr('src', pics[0])
-            $('#bild4').fadeIn(3000)
-            })
-            index++;
-    
-            
-            } 
-            else if ( index == 1){
-             
-                $('#bild4').fadeOut(3000 , function() {
-                $('#bild4').attr('src', pics[1])
-                $('#bild4').fadeIn(3000)
-                })
-                index++;
                 
-                }
-                else if ( index == 2){
-             
-                    $('#bild4').fadeOut(3000 , function() {
-                    $('#bild4').attr('src', pics[2])
-                    $('#bild4').fadeIn(3000)
-                    })
-                    index++;
-                    
-                    }    
-                else if ( index == 3){
-                        $('#bild4').fadeOut(3000 , function() {
-                        $('#bild4').attr('src', pics[3])
-                        $('#bild4').fadeIn(3000)
-                        })
-                        index= 0;
-                        
-                        }
-        }
     }
 
     var interval = null;
